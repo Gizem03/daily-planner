351ea0bdd31ef78cb6dc8b0fdf2172539970ea84
Gizem Unlu <unlu.sgizem@gmail.com>
2022-12-11 14:33:56 +0000

    future rows implemented

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,57 @@
-var currentDay = $("#currentDay");
-var currentDate = moment().format("dddd, MMMM Do");
-currentDay.html(currentDate);
-
-var currentTime = moment(09, "HH");
-var now = moment();
+// set the date
+function setHeadlineDate() {
+  var currentDay = $("#currentDay");
+  var currentDate = moment().format("dddd, MMMM Do");
+  currentDay.html(currentDate);
+}
 
 // TODO implement localstorage, use date, hour and text, function saveEvent
 // TODO read from localstorage, function readDay
 // TODO use fontawesome to use save icon in designs
 // TODO fix the row styling to be 100% width
-// TODO this might change after watching the video
 // TODO figure out where to use the timeBlock
-var timeBlock = $("<div>");
+// TODO finish up styling to match the mock gif
 
-var container = $(".container");
-container.append(timeBlock);
+function displayDailyPlanner() {
+  // TODO this might change after watching the video
+  var dailyPlanner = $("<div>");
 
-while (currentTime.hour() < 18) {
-  console.log(currentTime.format("hha"));
-  var row = $("<div>");
+  var container = $(".container");
+  container.append(dailyPlanner);
 
-  var hour = $("<div>");
-  hour.html(currentTime.format("hha"));
+  // time block rendering
+  var currentTime = moment(09, "HH");
+  var now = moment();
+  while (currentTime.hour() < 18) {
+    var row = $("<div>");
 
-  var btn = $("<button>");
-  btn.html("Save");
-  btn.addClass("saveBtn");
+    var hour = $("<div>");
+    hour.html(currentTime.format("hha"));
 
-  row.addClass("row");
-  row.append(hour);
-  var textarea = $("<textarea>");
-  row.append(textarea);
-  row.append(btn);
-  timeBlock.append(row);
+    var btn = $("<button>");
+    btn.html("Save");
+    btn.addClass("saveBtn");
 
-  var curHour = currentTime.hour();
-  var nowHour = now.hour();
-  if (curHour < nowHour) {
-    // row.addClass("future");
-    console.log("fooo");
-    textarea.addClass("past");
-  } else if (curHour === nowHour) {
-    textarea.addClass("present");
+    row.addClass("row");
+    row.append(hour);
+    var textarea = $("<textarea>");
+    row.append(textarea);
+    row.append(btn);
+    dailyPlanner.append(row);
+
+    var curHour = currentTime.hour();
+    var nowHour = now.hour();
+    if (curHour < nowHour) {
+      textarea.addClass("past");
+    } else if (curHour === nowHour) {
+      textarea.addClass("present");
+    } else {
+      textarea.addClass("future");
+    }
+
+    currentTime.add(1, "hours");
   }
-
-  // TODO handle future
-
-  currentTime.add(1, "hours");
 }
+
+setHeadlineDate();
+displayDailyPlanner();
